750437eb8f3012ae724881e68d7f9932755d40b2
saket219 <62448069+saket219@users.noreply.github.com>
2023-02-05 12:53:47 -0500

    Update MPG.js

--- MPG.js
+++ MPG.js
@@ -4,13 +4,11 @@
     var again = "y";
     
     do {
-      var miles = prompt("Enter miles driven"+
-      "Or enter 0 to end entries", 0);
+      var miles = prompt("Enter miles driven", 0);
       miles = parseFloat(miles);
       document.write('<h2> <i><center>Miles driven<i> <span id = "result"></span>'+miles+' </center></h2>');
       
-      var gallons = prompt("Enter gallons of gas used"+
-      "Or enter 0 to end entries", 0);
+      var gallons = prompt("Enter gallons of gas used", 0);
       gallons = parseFloat(gallons);
       document.write('<h2> <i><center>Gallons of gas used <i> <span id = "result"></span>'+gallons+' </center></h2>');
 
